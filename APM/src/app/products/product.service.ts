7319f679a28b22019586e785799079285b36ffc4
benjdaun <benjdaun@pop-os.localdomain>
2023-12-29 10:50:03 -0500

    proper product detail page working

--- APM/src/app/products/product.service.ts
+++ APM/src/app/products/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { IProduct } from "./product";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { Observable, catchError, tap, throwError } from "rxjs";
+import { Observable, catchError, map, tap, throwError } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
@@ -11,12 +11,18 @@ export class ProductService {
 
 private productUrl = 'api/products/products.json';
 constructor(private http: HttpClient) {}
+
 getProducts(): Observable<IProduct[]> {
     return this.http.get<IProduct[]>(this.productUrl).pipe(tap(data => console.log('All', JSON.stringify(data))),
     catchError(this.handleError)
     );
 }
-
+getProduct(id: number): Observable<IProduct | undefined> {
+  return this.getProducts()
+    .pipe(
+      map((products: IProduct[]) => products.find(p => p.productId === id))
+    );
+}
 private handleError(err: HttpErrorResponse) {
     // in a real world app, we may send the server to some remote logging infrastructure
     // instead of just logging it to the console
